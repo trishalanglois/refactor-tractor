df602610cdd9ea040fadc894c79d0f584c29b404
Cameron MacRae <cam.mac.cm@gmail.com>
2019-10-22 15:02:46 -0600

    Fix merge conflicts

--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -85,33 +85,31 @@ class ActivityRepository {
     return this.user.slice(index - 6, index + 1);
   }
 
-  getStepTrends(type) {
-    if (type === 'positive') {
-      return this.user.reduce((acc, day, index) => {
-        if (index < 2) {
-          return acc;
-        }
-        if ((day.numSteps > this.user[index - 1].numSteps) &&
-          (this.user[index - 1].numSteps > this.user[index - 2].numSteps)) {
-          acc.push(day.date);
-        }
+  getPositiveStepTrends() {
+    return this.user.reduce((acc, day, index) => {
+      if (index < 2) {
         return acc;
-      }, []);
-    } else {
-      return this.user.reduce((acc, day, index) => {
-        if (index < 2) {
-          return acc;
-        }
-        if ((day.numSteps < this.user[index - 1].numSteps) &&
-          (this.user[index - 1].numSteps < this.user[index - 2].numSteps)) {
-          acc.push(day.date);
-        }
-        return acc;
-        }, []);
       }
-    }
+      if ((day.numSteps > this.user[index - 1].numSteps) &&
+        (this.user[index - 1].numSteps > this.user[index - 2].numSteps)) {
+        acc.push(day.date);
+      }
+      return acc;
+    }, []);
   }
 
-
+  getNegativeStepTrends() {
+    return this.user.reduce((acc, day, index) => {
+      if (index < 2) {
+        return acc;
+      }
+      if ((day.numSteps < this.user[index - 1].numSteps) &&
+        (this.user[index - 1].numSteps < this.user[index - 2].numSteps)) {
+        acc.push(day.date);
+      }
+      return acc;
+    }, []);
+  }
+}
 
 export default ActivityRepository;
